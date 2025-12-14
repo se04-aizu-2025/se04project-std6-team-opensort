2a12cf3a7201566a935752a785bb6a16faa0e991
matthias-bock <148486256+matthias-bock@users.noreply.github.com>
2025-12-14 17:58:29 +0900

    Added scaffold for heap sort

---
+++ src/HeapSort.java
@@ -0,0 +1,13 @@
+package com.opensort.sorting;
+
+public class HeapSort extends SortingAlgorithm{
+
+    public HeapSort(int[] numbers){
+        super(numbers);
+    }
+
+    public int[] sort(){
+        return null;
+    }
+
+}
