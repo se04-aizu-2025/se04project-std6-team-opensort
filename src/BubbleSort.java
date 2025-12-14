8f65adf0883fd0ebfb99c7b0f405fecb0774d7d8
matthias-bock <148486256+matthias-bock@users.noreply.github.com>
2025-12-14 18:09:29 +0900

    Added scaffold for bubblesort

---
+++ src/BubbleSort.java
@@ -0,0 +1,13 @@
+package com.opensort.sorting;
+
+public class BubbleSort extends SortingAlgorithm{
+
+    public BubbleSort(int[] numbers){
+        super(numbers);
+    }
+
+    public int[] sort(){
+        return null;
+    }
+
+}
