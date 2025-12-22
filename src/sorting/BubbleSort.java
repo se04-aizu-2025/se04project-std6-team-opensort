fa16da917118c5dd2b2f2e54b6b3e0cbf5cfede7
matthias-bock <148486256+matthias-bock@users.noreply.github.com>
2025-12-22 20:44:25 +0900

    Added sorted event to bubblesort

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -23,6 +23,7 @@ public class BubbleSort extends SortingAlgorithm{
                     swap(j, j + 1);
                 }
             }
+            sorted(array.length - i - 1);
         }
 
         return array;
