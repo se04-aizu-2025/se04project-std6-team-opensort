dd0d4d771827ef3ec85df9237b36d16e28819d80
MithunThangaraj <133865250+MithunThangaraj@users.noreply.github.com>
2025-12-23 11:11:20 +0900

    added random sorted reverseSorted data generators

--- src/testing/DataGenerator.java
+++ src/testing/DataGenerator.java
@@ -2,35 +2,52 @@ package com.opensort.testing;
 
 import java.util.Random;
 
-public class DataGenerator {
+public class DataGenerator{
 
     private Random random;
 
-    public DataGenerator() {
+    public DataGenerator(){
         this.random = new Random();
     }
 
-    public int[] generateRandom(int size, int min, int max) {
+    public int[] generateRandom(int size, int min, int max){
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++){
+            arr[i] = random.nextInt(max - min + 1) + min;
+        }
+        return arr;
+    }
+
+    public int[] generateSorted(int size){
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++){
+            arr[i] = i;
+        }
+        return arr;
+    }
+
+
+    public int[] generateReverseSorted(int size){
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++){
+            arr[i] = size - i;
+        }
+        return arr;
+    }
+
+
+    public int[] generateWithDuplicates(int size, int uniqueValues){
         return null;
     }
 
-    public int[] generateSorted(int size) {
+    public int[] generateAllSame(int size, int value){
         return null;
     }
 
-    public int[] generateReverseSorted(int size) {
-        return null;
-    }
-
-    public int[] generateWithDuplicates(int size, int uniqueValues) {
-        return null;
-    }
-
-    public int[] generateAllSame(int size, int value) {
-        return null;
-    }
-
-    public int[] generateNearlySorted(int size, int swaps) {
+    public int[] generateNearlySorted(int size, int swaps){
         return null;
     }
 }
--- src/testing/TestEngine.java
+++ src/testing/TestEngine.java
@@ -2,13 +2,13 @@ package com.opensort.testing;
 
 import com.opensort.sorting.SortingAlgorithm;
 
-public class TestEngine {
+public class TestEngine{
 
-    public int[] runTest(SortingAlgorithm algorithm) {
+    public int[] runTest(SortingAlgorithm algorithm){
         return null;
     }
 
-    public long measureTime(SortingAlgorithm algorithm) {
+    public long measureTime(SortingAlgorithm algorithm){
         return 0L;
     }
 }
