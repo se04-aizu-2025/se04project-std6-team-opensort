086f2ca2966cb9d5b234620c4469986e32a7aaee
MithunThangaraj <133865250+MithunThangaraj@users.noreply.github.com>
2025-12-23 11:51:06 +0900

    implemented test engine

--- src/testing/TestEngine.java
+++ src/testing/TestEngine.java
@@ -2,13 +2,16 @@ package com.opensort.testing;
 
 import com.opensort.sorting.SortingAlgorithm;
 
-public class TestEngine{
+public class TestEngine {
 
-    public int[] runTest(SortingAlgorithm algorithm){
-        return null;
+    public int[] runTest(SortingAlgorithm algorithm) {
+        return algorithm.sort();
     }
 
-    public long measureTime(SortingAlgorithm algorithm){
-        return 0L;
+    public long measureTime(SortingAlgorithm algorithm) {
+        long start = System.nanoTime();
+        algorithm.sort();
+        long end = System.nanoTime();
+        return end - start;
     }
 }
