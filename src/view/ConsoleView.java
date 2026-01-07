c036f4bf09e25b9da05ff0c25d5714b4a1228547
matthias-bock <148486256+matthias-bock@users.noreply.github.com>
2026-01-07 15:41:34 +0900

    Added user controls to console view and extended the IView interface

--- src/view/ConsoleView.java
+++ src/view/ConsoleView.java
@@ -5,10 +5,18 @@ import com.opensort.sorting.events.MarkEvent;
 import com.opensort.sorting.events.SortEvent;
 import com.opensort.sorting.events.SwapEvent;
 
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 // Simple console view
 public class ConsoleView implements IView{
 
     private int[] array;
+    private String[] algorithms = {};
+    private ConcurrentLinkedQueue<SortEvent> eventsToProcess = new ConcurrentLinkedQueue<>();
+    private boolean running = true;
+    private Scanner scanner;
 
     // Print the current state of the array
     private void printArray(){
@@ -30,6 +38,11 @@ public class ConsoleView implements IView{
 
     @Override
     public void onSortEvent(SortEvent event) {
+        eventsToProcess.add(event);
+    }
+
+    // Function to handle a given sort event
+    private void handleEvent(SortEvent event){
         // Handle the different types of sorting events
         switch (event){
             case SwapEvent swap -> {
@@ -57,7 +70,142 @@ public class ConsoleView implements IView{
     @Override
     public void setArray(int[] array) {
         this.array = array;
+    }
+
+    @Override
+    public void setAlgorithms(String[] algorithms) {
+        this.algorithms = algorithms;
+    }
+
+    // Get a new array from user input
+    private int[] getArrayFromUser(){
+        int[] newArray;
+        boolean inputOK;
+        // Repeat until the user input is ok
+        do {
+            inputOK = true;
+
+            System.out.print("Please enter a ',' separated list of integers: ");
+            String[] list = scanner.nextLine().split(",");
+
+            // Try to convert the individual numbers
+            newArray = new int[list.length];
+            for (int i = 0; i < list.length; i++) {
+                String number = list[i];
+                try {
+                    newArray[i] = Integer.parseInt(number);
+                } catch (NumberFormatException _) {
+                    // Print conversion errors and mark the input as not ok
+                    System.out.printf("Error at index %d. '%s' is not a number.\n", i, number);
+                    inputOK = false;
+                }
+            }
+        } while (!inputOK);
+
+        return newArray;
+    }
+
+    // Get a new sorting algorithm from user input
+    public int getAlgorithmFromUser(){
+        if(algorithms.length == 0){
+            System.out.println("No algorithms available.");
+            return -1;
+        }
+
+        int algorithm = -1;
+
+        // Repeat until a valid algorithm is selected
+        do {
+            // Print all options
+            for (int i = 0; i < algorithms.length; i++) {
+                System.out.printf("%d: %s\n", i, algorithms[i]);
+            }
+            System.out.printf("Select an algorithm (0-%d): \n", algorithms.length - 1);
+
+            // Try to convert user input
+            try {
+                algorithm = scanner.nextInt();
+            } catch (Exception _) {
+                System.out.println("Invalid input.");
+            }
+            // Check bounds of algorithm array
+        } while(algorithm < 0 || algorithm > algorithms.length);
+
+        return algorithm;
+    }
+
+    private void printHelp(){
+        System.out.println("""
+Available commands:
+    - (n)ext:
+        Perform next step
+    - (h)elp | ?:
+        Print this help
+    - (q)uit | (e)xit:
+        Quit the program
+    - algo:
+        Change the sorting algorithm
+    - arr:
+        Change the array being sorted
+                            """);
+    }
+
+    @Override
+    public void run() {
+        scanner = new Scanner(System.in);
+
+        if(array == null){
+            getArrayFromUser();
+        }
+
+        getAlgorithmFromUser();
+
         System.out.print("Initial array:\t\t");
         printArray();
+
+        String lastInput = "";
+        while (running){
+            System.out.print("(h for help)>_: ");
+            String userInput = scanner.nextLine();
+
+            if (userInput.isBlank()) userInput = lastInput;
+            lastInput = userInput;
+
+            switch (userInput.strip()){
+                case "q":
+                case "quit":
+                case "e":
+                case "exit":
+                    running = false;
+                    break;
+                case "n":
+                case "next":
+                    SortEvent nextEvent = eventsToProcess.poll();
+                    if(nextEvent != null){
+                        handleEvent(nextEvent);
+                    } else {
+                        System.out.println("Nothing to do...");
+                    }
+                    break;
+                case "h":
+                case "help":
+                case "?":
+                    printHelp();;
+                    break;
+                case "algo":
+                    int algorithm = getAlgorithmFromUser();
+                    if(algorithm >= 0){
+                        System.out.println(algorithms[algorithm]);
+                    }
+                    break;
+                case "arr":
+                    int[] newArray = getArrayFromUser();
+                    System.out.println(Arrays.toString(newArray));
+                    break;
+                default:
+                    printHelp();
+                    break;
+            }
+        }
     }
 }
--- src/view/IView.java
+++ src/view/IView.java
@@ -3,11 +3,13 @@ package com.opensort.view;
 import com.opensort.sorting.events.SortEvent;
 
 // Interface to generalize the view
-public interface IView {
+public interface IView extends Runnable {
 
     // Allow the view to react to events from the currently used sorting algorithm
     public void onSortEvent(SortEvent event);
 
     // Tell the view what array it should display
     public void setArray(int[] array);
+
+    public void setAlgorithms(String[] algorithms);
 }
