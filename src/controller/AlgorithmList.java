17f252d945e3e8ed1b4dea04e774112af0b7a1c5
matthias-bock <148486256+matthias-bock@users.noreply.github.com>
2026-01-14 19:41:11 +0900

    Added documentation

--- src/controller/AlgorithmList.java
+++ src/controller/AlgorithmList.java
@@ -6,7 +6,12 @@ import com.opensort.sorting.SortingAlgorithm;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 
+// This class contains a list of all algorithms currently implemented
 public class AlgorithmList {
+
+    // List of all implemented algorithms
+    // To add a new algorithm to  the software, append it to this array
+    // The ID of the algorithm referenced in the software is the index of the algorithm in this array
     public static final AlgorithmData[] algorithms = {
         new AlgorithmData(){{
             name = "Bubble sort";
@@ -14,15 +19,19 @@ public class AlgorithmList {
         }}
     };
 
+    // Get the names of all algorithms as a string array
     public static String[] getStrings(){
         return Arrays.stream(algorithms).map(algorithm -> algorithm.name).toArray(String[]::new);
     }
 
+    // Create a new instance of a sorting algorithm with the given array
     public static SortingAlgorithm build(int id, int[] array) throws InvocationTargetException, InstantiationException, IllegalAccessException {
         return (SortingAlgorithm) algorithms[id].classType
                 .getDeclaredConstructors()[0].newInstance((Object)array);
     }
 
+    // Get the index of an algorithm from its name
+    // Returns -1 if there is no algorithm with the given name
     public static int getIndex(String name){
         for(int i = 0; i < algorithms.length; i++){
             if(algorithms[i].name.equals(name)){
