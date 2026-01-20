8d8ffda5bd1302757193278d962f7e7eefb91e28
MithunThangaraj <133865250+MithunThangaraj@users.noreply.github.com>
2026-01-20 14:08:29 +0900

    Test engine

--- src/testing/TestEngine.java
+++ src/testing/TestEngine.java
@@ -1,11 +1,64 @@
-package com.opensort.testing;
+package com. opensort.testing;
 
 import com.opensort.controller.AlgorithmList;
-import com.opensort.sorting.SortingAlgorithm;
+import com.opensort.controller.IController;
+import com.opensort.sorting. SortingAlgorithm;
+import com. opensort.sorting.events.*;
+import com.opensort.view. IView;
 
 public class TestEngine {
 
     DataGenerator generator = new DataGenerator();
+
+    public static class TestResult{
+        public String algorithmName;
+        public boolean[] testsPassed;
+        public long[] executionTimes;
+        public int[] compareCounts;
+        public int[] swapCounts;
+
+        public TestResult(String algorithmName, boolean[] testsPassed, long[] executionTimes, int[] compareCounts, int[] swapCounts){
+            this.algorithmName = algorithmName;
+            this.testsPassed = testsPassed;
+            this.executionTimes = executionTimes;
+            this.compareCounts = compareCounts;
+            this.swapCounts = swapCounts;
+        }
+    }
+
+    private static class EventCounter implements IView{
+        int compareCount = 0;
+        int swapCount = 0;
+
+        @Override
+        public void onSortEvent(SortEvent event) {
+            if (event instanceof CompareEvent){
+                compareCount++;
+            }else if (event instanceof SwapEvent){
+                swapCount++;
+            }
+        }
+
+        @Override
+        public void setArray(int[] array){}
+
+        @Override
+        public void setAlgorithms(String[] algorithms){}
+
+        @Override
+        public void addEventListener(IController listener){}
+
+        @Override
+        public void removeEventListener(IController listener){}
+
+        public void reset(){
+            compareCount = 0;
+            swapCount = 0;
+        }
+
+        @Override
+        public void run(){}
+    }
 
     public boolean runTest(SortingAlgorithm algorithm){
         int[] arr = algorithm.sort();
@@ -17,31 +70,57 @@ public class TestEngine {
         return true;
     }
 
-    public void runAll(){
+    public TestResult runTestsForAlgorithm(int algorithmIndex, TestData[] testCases) {
+        String algorithmName = AlgorithmList. algorithms[algorithmIndex].name;
+        boolean[] results = new boolean[testCases.length];
+        long[] executionTimes = new long[testCases.length];
+        int[] compareCounts = new int[testCases.length];
+        int[] swapCounts = new int[testCases. length];
+        EventCounter counter = new EventCounter();
+
+        for (int j = 0; j < testCases.length; j++) {
+            try {
+                counter.reset();
+                SortingAlgorithm algorithm = AlgorithmList.build(algorithmIndex, testCases[j].data.clone());
+                algorithm.addEventListener(counter);
+
+                long startTime = System.nanoTime();
+                boolean result = runTest(algorithm);
+                long endTime = System.nanoTime();
+
+                results[j] = result;
+                executionTimes[j] = endTime - startTime;
+                compareCounts[j] = counter.compareCount;
+                swapCounts[j] = counter.swapCount;
+            } catch (Exception e) {
+                results[j] = false;
+                executionTimes[j] = 0;
+                compareCounts[j] = 0;
+                swapCounts[j] = 0;
+            }
+        }
+
+        return new TestResult(algorithmName, results, executionTimes, compareCounts, swapCounts);
+    }
+
+    public TestResult[] runAll() {
+        return runAll(10);
+    }
+
+    public TestResult[] runAll(int arraySize) {
         TestData[] testCases = {
-                new TestData("Sorted", generator.generateSorted(10)),
-                new TestData("Reverse", generator.generateReverseSorted(10)),
-                new TestData("Random", generator.generateRandom(10, 1, 50))
+                new TestData("Sorted", generator.generateSorted(arraySize)),
+                new TestData("Reverse", generator.generateReverseSorted(arraySize)),
+                new TestData("Random", generator. generateRandom(arraySize, 1, 50))
         };
 
-        System.out.printf("%-15s %-8s %-8s %-8s\n",
-                "Algorithm", "Sorted", "Reverse", "Random");
-        System.out.println("-".repeat(40));
+        TestResult[] results = new TestResult[AlgorithmList. algorithms.length];
 
-        for(int i = 0; i < AlgorithmList.algorithms.length; i++){
-            System.out.printf("%-15s ", AlgorithmList.algorithms[i].name);
-
-            for(TestData testCase : testCases){
-                try {
-                    SortingAlgorithm algorithm = AlgorithmList.build(i, testCase.data.clone());
-                    boolean result = runTest(algorithm);
-                    System.out.printf("%-8s ", result);
-                } catch (Exception e) {
-                    System.out.printf("%-8s ", "ERROR");
-                }
-            }
-            System.out.println();
+        for (int i = 0; i < AlgorithmList.algorithms.length; i++){
+            results[i] = runTestsForAlgorithm(i, testCases);
         }
+
+        return results;
     }
 
     public long measureTime(SortingAlgorithm algorithm){
@@ -49,26 +128,6 @@ public class TestEngine {
         algorithm.sort();
         long end = System.nanoTime();
         return end - start;
-    }
-
-    public EventCounts countEvents(SortingAlgorithm algorithm){
-
-        return new EventCounts(0, 0);
-    }
-
-    public static class EventCounts{
-        public final int compareCount;
-        public final int swapCount;
-
-        public EventCounts(int compareCount, int swapCount){
-            this.compareCount = compareCount;
-            this.swapCount = swapCount;
-        }
-
-        @Override
-        public String toString(){
-            return String.format("Compares: %d, Swaps: %d", compareCount, swapCount);
-        }
     }
 
     private static class TestData{
